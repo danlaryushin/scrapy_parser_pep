0c1b694139942574b693811130d9212878a57ccf
unknown <danlaryushin@gmail.com>
2023-06-14 09:32:25 +0300

    After review

--- pep_parse/middlewares.py
+++ pep_parse/middlewares.py
@@ -1,21 +1,13 @@
 from scrapy import signals
 
 
-class PepParseBase:
+class PepParseSpiderMiddleware:
     @classmethod
     def from_crawler(cls, crawler):
         s = cls()
         crawler.signals.connect(s.spider_opened, signal=signals.spider_opened)
         return s
 
-    def process_exception(self, request, exception, spider):
-        pass
-
-    def spider_opened(self, spider):
-        spider.logger.info('Spider opened: %s' % spider.name)
-
-
-class PepParseSpiderMiddleware(PepParseBase):
     def process_spider_input(self, response, spider):
         return None
 
@@ -23,14 +15,23 @@ class PepParseSpiderMiddleware(PepParseBase):
         for i in result:
             yield i
 
+    def process_spider_exception(self, response, exception, spider):
+        pass
+
     def process_start_requests(self, start_requests, spider):
         for r in start_requests:
             yield r
 
+    def spider_opened(self, spider):
+        spider.logger.info('Spider opened: %s' % spider.name)
 
-class PepParseDownloaderMiddleware(PepParseBase):
+
+class PepParseDownloaderMiddleware(PepParseSpiderMiddleware):
     def process_request(self, request, spider):
         return None
 
     def process_response(self, request, response, spider):
         return response
+
+    def process_exception(self, request, exception, spider):
+        pass
