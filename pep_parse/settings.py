b8dfd1f361850be0c92554c28424a5e24202072d
unknown <danlaryushin@gmail.com>
2023-06-14 09:28:56 +0300

    After review

--- pep_parse/settings.py
+++ pep_parse/settings.py
@@ -3,11 +3,12 @@ from pathlib import Path
 BASE_DIR = Path(__file__).parent.parent
 RESULTS_DIR = 'results'
 DT_FORMAT = '%Y-%m-%d_%H-%M-%S'
+ALLOWED_DOMAIN = 'peps.python.org'
 
-BOT_NAME = "pep_parse"
+BOT_NAME = 'pep_parse'
 
-SPIDER_MODULES = ["pep_parse.spiders"]
-NEWSPIDER_MODULE = "pep_parse.spiders"
+SPIDER_MODULES = ['pep_parse.spiders']
+NEWSPIDER_MODULE = 'pep_parse.spiders'
 
 
 ROBOTSTXT_OBEY = True
--- pep_parse/spiders/pep.py
+++ pep_parse/spiders/pep.py
@@ -1,24 +1,28 @@
 import scrapy
 
 from pep_parse.items import PepParseItem
+from ..settings import ALLOWED_DOMAIN
 
 
 class PepSpider(scrapy.Spider):
-    name = "pep"
-    allowed_domains = ["peps.python.org"]
-    start_urls = ["https://peps.python.org/"]
+    name = 'pep'
+    allowed_domains = ALLOWED_DOMAIN
+    start_urls = [f'https://{allowed_domains}/']
 
     def parse(self, response):
         index = response.css('section[id="numerical-index"]')
         pep_links = index.css('a[href^="pep-"]')
-        for pep_link in pep_links[20:]:
+        for pep_link in pep_links:
             yield response.follow(pep_link, callback=self.parse_pep)
 
     def parse_pep(self, response):
         info = response.css('h1.page-title::text').get().split(' – ')
+        number = info[0].split()[-1]
+        name = info[-1]
+        status = response.css('abbr::text').get()
         data = {
-            'number': info[0].split()[-1],
-            'name': info[-1],
-            'status': response.css('abbr::text').get()
+            'number': number,
+            'name': name,
+            'status': status
         }
         yield PepParseItem(data)
